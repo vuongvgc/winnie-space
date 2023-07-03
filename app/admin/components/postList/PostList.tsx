c516d7ef0693e2a430fc877e9dd8246a8e8f85d3
vuong96 <minhvuong.do@manabie.com>
2023-07-03 21:54:00 +0700

    feat: add api post

--- app/admin/components/postList/PostList.tsx
+++ app/admin/components/postList/PostList.tsx
@@ -1,14 +1,26 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import prisma from '@/lib/prisma'
+import { getServerSession } from 'next-auth'
 import React from 'react'
 export default async function PostList() {
-  const posts = await prisma.post.findMany()
+  const session = await getServerSession(authOptions)
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      email: session?.user?.email || ''
+    }
+  })
+  const posts = await prisma.post.findMany({
+    where: {
+      authorId: currentUser?.id || ''
+    }
+  })
   return (
     <div>
       <h1>PostList</h1>
       {posts.map((post) => {
         return (
           <p key={post.id}>
-            {post.title} - {post.content}
+            {post.title} - {post.content?.toString()}
           </p>
         )
       })}
--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
+import { authOptions } from '../auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
 export async function GET() {
   const posts = await prisma.post.findMany()
   return NextResponse.json(posts)
@@ -7,10 +9,17 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
   const { title, content } = await req.json()
+  const session = await getServerSession(authOptions)
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session?.user?.email || ''
+    }
+  })
   const post = await prisma.post.create({
     data: {
       title,
-      content
+      content,
+      authorId: user?.id || ''
     }
   })
 
